ba6e58976037de7e67d892963dececc35d0335c0
Felipe Mohr <felipe18mohr@gmail.com>
2020-07-27 16:39:28 -0300

    Finished joy control

--- i3e-d3_control/src/joy_teleop.cpp
+++ i3e-d3_control/src/joy_teleop.cpp
@@ -1,3 +1,30 @@
+// Teleop configuration for a generic joystick
+// 'L3' up/down for linear velocity, left/right for angular velocity
+// 'L1' or 'R1' to stop the robot
+//  Hold '3' to enable turbo
+//  Hold '1' to move slowly
+//
+//           L2                                       R2
+//          L1                                       R1
+//       _=====_                                  _=====_
+//      / _____ \                                / _____ \
+//    +.-'_____'-.------------------------------.-'_____'-.+
+//   /   |     |  '.        X X X X           .'  |     |   \
+//  / ___| /|\ |___ \                        / ___|  1  |___ \      
+// / |      |      | ;    |9|       |10 >   ; |             ||
+// | | <---   ---> | |                      | | 4         2 ||       
+// | |___   |   ___| ;                      ; |___       ___||
+// |\    | \|/ |    /  _      ____      _   \    |  3  |    /|      
+// | \   |_____|  .','" "',  (MODE)  ,'" "', '.  |_____|  .' |
+// |  '-.______.-' /       \        /       \  '-._____.-'   |
+// |               |  L3   |--------|  R3   |                |
+// |              /\       /        \       /\               |
+// |             /  '.___.'          '.___.'  \              |
+// |            /                              \             |
+//  \          /                                \           /
+//   \________/                                  \_________/
+//
+
 #include <ros/ros.h>
 #include <geometry_msgs/Twist.h>
 #include <sensor_msgs/Joy.h>
@@ -5,14 +32,20 @@
 class TeleopD3{
 public:
     TeleopD3();
+    void moveD3();
     
 private:
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
 
+    geometry_msgs::Twist twist_;
     ros::NodeHandle nh_;
 
+    int reverse_;
+    bool rev_;
+    int stop1_, stop2_;
+    int turbo_, slow_;
     int linear_, angular_;
-    double l_scale_, a_scale_;
+    double l_scale_, a_scale_, k_;
     ros::Publisher vel_pub_;
     ros::Subscriber joy_sub_;
 
@@ -22,21 +55,39 @@ TeleopD3::TeleopD3(): linear_(1), angular_(0){
 
     nh_.param("axis_linear", linear_, linear_);
     nh_.param("axis_angular", angular_, angular_);
+    nh_.param("stop_button1", stop1_, stop1_);
+    nh_.param("stop_button2", stop2_, stop2_);
+    nh_.param("turbo_button", turbo_, turbo_);
+    nh_.param("slow_button", slow_, slow_);
     nh_.param("scale_linear", l_scale_, l_scale_);
     nh_.param("scale_angular", a_scale_, a_scale_);
+    nh_.param("reverse_button", reverse_, reverse_);
+
+    rev_ = true;
 
     vel_pub_ = nh_.advertise<geometry_msgs::Twist>("/i3e_d3_velocity_controller/cmd_vel", 1);
-
     joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &TeleopD3::joyCallback, this);
 
 }
 
 void TeleopD3::joyCallback(const sensor_msgs::Joy::ConstPtr& joy){
+
+    if(joy->buttons[reverse_]) rev_ = !rev_;
     
-    geometry_msgs::Twist twist;
-    twist.linear.x = l_scale_*joy->axes[linear_];
-    twist.angular.z = a_scale_*joy->axes[angular_];
-    vel_pub_.publish(twist);
+    k_ = 0.4;
+    if(joy->buttons[turbo_]) k_ = 1;
+    if(joy->buttons[slow_]) k_ = 0.2;
+    if(joy->buttons[stop1_] || joy->buttons[stop2_]) k_ = 0;
+    
+    twist_.linear.x = k_*l_scale_*joy->axes[linear_];
+    
+    if(joy->axes[linear_] < 0 && rev_) k_ = -k_;
+    twist_.angular.z = k_*a_scale_*joy->axes[angular_];
+
+}
+
+void TeleopD3::moveD3(){    
+    vel_pub_.publish(twist_); 
 }
 
 
@@ -45,5 +96,11 @@ int main(int argc, char *argv[]){
     ros::init(argc, argv, "/teleop_i3e_d3");
     TeleopD3 teleop_d3;
 
-    ros::spin();
+    ros::Rate loop_rate(10);
+    while (ros::ok()){
+        teleop_d3.moveD3();
+
+        ros::spinOnce();
+        loop_rate.sleep();
+    }
 }
